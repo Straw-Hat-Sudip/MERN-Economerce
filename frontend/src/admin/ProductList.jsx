9e0c57c73e021b94607a4e3eba556a93c0c63485
sudip <sudippatra695@gmail.com>
2026-04-20 12:33:33 +0530

    Update ProductList.jsx
    Removed commented-out code for the product list display.

--- frontend/src/admin/ProductList.jsx
+++ frontend/src/admin/ProductList.jsx
@@ -2,7 +2,7 @@ import {useState, useEffect} from 'react'
 import api from "../api/axios"
 import { Link } from 'react-router'
 import { Plus, ShoppingCart, Edit3, Trash2, Package, Heart } from "lucide-react";
-// import { deleteProduct } from '../../../backend/src/controllers/productController';
+
 
 const ProductList = () => {
 
@@ -26,45 +26,6 @@ const ProductList = () => {
     useEffect(()=>{
         loadProduct();
     },[])
-
-
-//   return (
-//     <div className='max-w-4xl mx-auto mt-10'>
-//         <div className='flex justify-between items-center mb-6'>
-//             <h2 className='text-2xl font-bold'>Product List</h2>
-//             <Link to='/admin/products/add' className='bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600'>Add New Product</Link>
-//         </div> 
-
-//         <table className='w-full table-auto border-collapse border border-gray-200'>
-//             <thead>
-//                 <tr className='bg-gray-100'>
-//                     <th className='border border-gray-200 px-4 py-2'>Title</th>
-//                     <th className='border border-gray-200 px-4 py-2'>Price</th>
-//                     <th className='border border-gray-200 px-4 py-2'>Stock</th>
-//                     <th className='border border-gray-200 px-4 py-2'>Action</th>
-//                 </tr>
-//             </thead>
-
-//             <tbody>
-//                 {products.map((product)=>(
-//                     <tr key={product.id} className='text-center'>
-//                         <td className='border border-gray-200 px-4 py-2'>{product.title}</td>
-//                         <td className='border border-gray-200 px-4 py-2'>{product.price}</td>
-//                         <td className='border border-gray-200 px-4 py-2'>{product.stock}</td>
-//                         <td className='border border-gray-200 px-4 py-2'>
-//                             <Link to={`/admin/products/edit/${product._id}`} className=' text-blue-500 hover:underline px-4 py-2'>
-//                                 Edit
-//                             </Link>
-//                             <button onClick={()=>deleteProduct(product._id)} className='text-red-500 hover:underline'>
-//                                 Delete
-//                             </button>
-//                         </td>
-//                     </tr>
-//                 ))}
-//             </tbody>
-//         </table> 
-//     </div>
-//   )
 
 return (
   <div className="max-w-6xl mx-auto mt-12 px-6 pb-20">
